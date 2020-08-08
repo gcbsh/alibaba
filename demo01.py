aae2ccdab91646fc6e2141f7981065942e1d809a
YaoMeng <1126895915@qq.com>
2020-08-08 16:35:08 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,10 @@
+print(type("你好,hellow world"))
+print(type(2333333))
+print(type(True) ) #布尔类型
+print(type(()))    #元组
+print(type([])  ) #数组
+print(type({}) )  #字典
+"""多行注释"""
+a = (input("请输入:"))
+b = (input("请输入:"))
+print("input获取的内容:",len(a+b))
